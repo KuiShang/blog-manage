ceb2fd3cd65c183f1fcc0ccbcd7d0068b84a7359
KuiShang <kuishang@outlook.com>
2017-12-18 22:26:18 +0800

    manager管理菜单ing

--- src/pages/manage/manage.vue
+++ src/pages/manage/manage.vue
@@ -4,7 +4,34 @@
         <el-menu 
         class="nav-menu" 
         background-color="#545c64"
+        text-color="#fff"
+        router = true
         >
+          <div class="header-logo">
+            <h2>Manager</h2>
+          </div>
+
+          <el-menu-item 
+          v-if="!item.children"
+          v-for="(item, index) in menuItems" :key="index"
+          :index="item.id" 
+          >
+            {{item.text}}
+          </el-menu-item>
+
+          <el-submenu v-else
+          v-for="(item, index) in menuItems" :key="index"
+          :index="item.id"
+          >
+            <template slot="title">{{item.text}}</template>
+            <el-menu-item
+            v-for="(child, index) in item.children" :key="index"
+            :index="child.id"
+            >
+              {{child.text}}
+            </el-menu-item>
+
+          </el-submenu>
 
         </el-menu>
      </el-col>
@@ -12,18 +39,24 @@
    </el-row>
 </template>
 <script>
+import menus from './menus'
 export default {
   name: 'manage',
   data () {
     return {
+      menuItems: menus,
     }
   }
 
 }
 </script>
 <style lang="scss" scoped>
+@import "../../style/var.scss";
 .nav{
   height: 100%;
+  .nav-menu {
+     height: 100%;
+  }
 }
 
 
